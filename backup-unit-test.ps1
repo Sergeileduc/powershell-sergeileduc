b3096ffecbddd9d70480b04df8d5fae747a1042b
Sergeileduc <sergei.leduc@gmail.com>
2025-11-18 15:38:14 +0100

    Refactor unit test

--- backup-unit-test.ps1
+++ backup-unit-test.ps1
@@ -1,10 +1,15 @@
+# Variables perso
+$devPath = Join-Path -Path $env:USERPROFILE -ChildPath "Dev"
+
 # Chemin vers le dossier OneDrive Documents
 $oneDriveDocs = Join-Path "$env:USERPROFILE\OneDrive\Documents" "Scripts\Powershell"
 # Importe les fonctions
 Import-Module (Join-Path $oneDriveDocs "SergeBackup")
 
-# Dossier temporaire local
-$staging = Init-StagingFolder -customPath "$env:USERPROFILE"
+
+# Dossier local
+$backupFolder = Init-BackupFolder
+Write-Host "📂 Dossier de backup créé : $backupFolder" -ForegroundColor Cyan
 
 
 # 1. Chocolatey
@@ -12,9 +17,9 @@ $tempChocoExport = Join-Path $env:TEMP "packages-choco.config"
 choco export --include-version-numbers $tempChocoExport
 
 if (Test-Path $tempChocoExport) {
-  Save $tempChocoExport -targetPath "$staging\packages-choco.config"
-  Write-Host "✅ Chocolatey exporté et copié dans les deux backups" -ForegroundColor Green
+  Save $tempChocoExport -targetPath "$backupFolder\packages-choco.config"
+  Write-Host "✅ Chocolatey exporté" -ForegroundColor Green
   Remove-Item $tempChocoExport
 } else {
-  Write-Host "❌ Échec de l’export Chocolatey — fichier introuvable : $tempChocoExport" -ForegroundColor Red
+  Write-Host "❌ Échec de l'export Chocolatey — fichier introuvable : $tempChocoExport" -ForegroundColor Red
 }
